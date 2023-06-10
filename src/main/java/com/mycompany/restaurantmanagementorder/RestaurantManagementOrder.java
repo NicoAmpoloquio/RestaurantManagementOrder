aa064887dfffff7274ea4f1e23df36a91fd00d5a
Nico Ampoloquio <116412203+NicoAmpoloquio@users.noreply.github.com>
2023-06-10 09:58:25 +0800

    Separating every class.

---
+++ src/main/java/com/mycompany/restaurantmanagementorder/RestaurantManagementOrder.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantmanagementorder;
+
+/**
+ *
+ * @author Nico
+ */
+public class RestaurantManagementOrder {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
